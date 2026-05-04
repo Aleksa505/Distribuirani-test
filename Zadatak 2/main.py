1e1c6c37d7bea90c73fc189669f8066f752895cc
AleksaBober <aleksabogosavljevic03@gmail.com>
2026-05-04 17:13:07 +0200

    innital commit

---
+++ Zadatak 2/main.py
@@ -0,0 +1,14 @@
+def outer_func():
+    message = "Hello world"
+
+    def inner_func():
+        print(message)
+
+    return inner_func
+
+outer_func()
+
+my_func = outer_func()
+print(my_func)
+print(my_func.__name__)
+#my_func()
